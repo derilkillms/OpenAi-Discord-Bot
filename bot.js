8e5bef4ebbba21fee50060e4aba9f5b51ab569c7
Muhammad Deril <49135753+derilkillms@users.noreply.github.com>
2023-01-09 13:20:55 +0700

    add_generate_image

--- bot.js
+++ bot.js
@@ -1,25 +1,29 @@
 
-const { ask } = require("./ai.js");
+const { ask, generateImg } = require("./ai.js");
 require('dotenv').config()
-const token = process.env.TOKEN
+const token = process.env.DISCORD_TOKEN
 const { Client, Events, GatewayIntentBits } = require('discord.js')
 const client = new Client({
-    intents:
-      [GatewayIntentBits.Guilds,
-      GatewayIntentBits.GuildMessages,
-      GatewayIntentBits.MessageContent]
-  });
-  
-  client.once(Events.ClientReady, c => {
-    console.log(`Ready! Logged in as ${c.user.tag}`);
-  });
-  
-  client.on(Events.MessageCreate, async message => {
-    if (message.content.substring(0, 1) === "!") {
-      const prompt = message.content.substring(1); 
-      const answer = await ask(prompt); 
-      client.channels.fetch(message.channelId).then(channel => channel.send(answer));
-    }
-  });
-  
-  client.login(token);
+  intents:
+  [GatewayIntentBits.Guilds,
+  GatewayIntentBits.GuildMessages,
+  GatewayIntentBits.MessageContent]
+});
+
+client.once(Events.ClientReady, c => {
+  console.log(`Ready! Logged in as ${c.user.tag}`);
+});
+
+client.on(Events.MessageCreate, async message => {
+  if (message.content.substring(0, 5) == "!chat") {
+    const prompt = message.content.substring(6); 
+    const answer = await ask(prompt); 
+    client.channels.fetch(message.channelId).then(channel => channel.send(answer));
+  }else if (message.content.substring(0, 6) == "!image") {
+    const prompt = message.content.substring(7); 
+    const answer = await generateImg(prompt); 
+    client.channels.fetch(message.channelId).then(channel => channel.send(answer));
+  }
+});
+
+client.login(token);
